9c919e4d48c19b5d139af4e8055b90a53bc4cde0
Ryota <teamgame4410@gmail.com>
2015-02-01 22:48:53 +0900

    ロジック変更

--- src/minesweeper/GameBase.java
+++ src/minesweeper/GameBase.java
@@ -1,9 +1,13 @@
 package minesweeper;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GameBase {
+
 	private final static int MIN_SIDE_LENGTH = 5;  // 一辺の長さの最低値
 	private final static int MAX_SIDE_LENGTH = 10; // 一辺の長さの最大値
 	private final static int MIN_BOMB_AMOUNT = 5;  // 爆弾の最低数
@@ -12,10 +16,9 @@ public class GameBase {
 
 	public GameBase(){}
 
-	public PlayBoard makeBoard()
+	// マップを生成するクラス
+	private PlayBoard makeBoard()
 	{
-		showTitle();
-
 		int sideLength = inputSideLength();
 		int bombAmount = inputBombAmount();
 		PlayBoard playBoard = new PlayBoard(sideLength, bombAmount);
@@ -23,42 +26,77 @@ public class GameBase {
 		return playBoard;
 	}
 
-
 	public void playGame()
 	{
+		// タイトルの表示
+		showTitle();
+		// マップの生成
 		PlayBoard playBoard = makeBoard();
 
+		// メインループ
 		while ( ! playBoard.isClear())
 		{
 			playBoard.printMap();
-			inputNextAction(playBoard);
+			String input = inputNextPosition(playBoard);
+
+			int xCordinate = (int)(input.charAt(0) - 'a');
+			int yCordinate = Integer.parseInt(input.substring(1,2));
+			openSquareRecursive(xCordinate, yCordinate, playBoard);
 		}
 
+		// 終了処理
 		endGame(playBoard);
 	}
 
-	private void inputNextAction(PlayBoard playBoard)
-	{
-		 System.out.println();
-		 System.out.println("空けたい場所を選んでくださいな ( 例：a2 ）");
 
-		 try
-		 {
-			 String str = br.readLine();
-			 int width  = Integer.parseInt(str.substring(0,1));
-			 int height = Integer.parseInt(str.substring(1,2));
-			 openSquareRecursive(height, width, playBoard);
-		 }
-		 catch (Exception e)
-		 {
-			 System.out.println("入力エラーです。");
-		 }
+	private String inputNextPosition(PlayBoard playBoard)
+	{
+		String inputString = "";
+		System.out.println();
+		//System.out.println("空けたい場所を選択してください ( 例：a2 ）");
+
+		try
+		{
+			do
+			{
+				System.out.println("空けたい場所を選択してください ( 例：a2 ）");
+				inputString = br.readLine();
+			}
+			while ( ! isValidInput(inputString, playBoard));
+
+			return inputString;
+		}
+		catch (IOException e)
+		{
+			System.out.println("入力エラーです。もう一度入力してください");
+			System.exit(-1);
+		}
+		return inputString;
+	}
+
+	private boolean isValidInput(String input, PlayBoard playBoard)
+	{
+		char a = 'a' - 1;
+		for  (int i = 0; i < playBoard.getSideLength(); i++)
+		{
+			a++;
+		}
+
+		Pattern p = Pattern.compile("[a-" + (char)(a - 1) + "][0-" + (playBoard.getSideLength() - 1) +"]");
+		Matcher m = p.matcher(input);
+
+		if ( ! m.find())
+		{
+			return false;
+		}
+		return true;
 	}
 
 	private void endGame(PlayBoard playBoard)
 	{
 		playBoard.printMap();
 
+		System.out.println();
 		System.out.println("####################################");
 		System.out.println("#                                  #");
 		System.out.println("#       おめでとうございます       #");
@@ -86,7 +124,7 @@ public class GameBase {
 		while (sideLength < MIN_SIDE_LENGTH || MAX_SIDE_LENGTH < sideLength)
 		{
 			 System.out.println();
-			 System.out.println("５×５～１０×１０までサイズを選べます");
+			 System.out.println(MIN_SIDE_LENGTH + "×" + MIN_SIDE_LENGTH + "～" + MAX_SIDE_LENGTH + "×" + MAX_SIDE_LENGTH + "までサイズを選べます");
 			 System.out.println("プレイしたいサイズを入力してください");
 
 			 try
@@ -110,7 +148,7 @@ public class GameBase {
 		while (bombAmount < MIN_BOMB_AMOUNT || MAX_BOMB_AMOUNT < bombAmount)
 		{
 			 System.out.println();
-			 System.out.println("５～２０まで爆弾の個数を選べます");
+			 System.out.println(MIN_BOMB_AMOUNT + "～" + MAX_BOMB_AMOUNT + "まで爆弾の個数を選べます");
 			 System.out.println("設置したい爆弾の個数を入力してください");
 
 			 try
@@ -130,7 +168,6 @@ public class GameBase {
 	// 周りのところもあける
 	private void openSquareRecursive(int xCordinate, int yCordinate, PlayBoard playBoard)
 	{
-		System.out.println(xCordinate + ":" + yCordinate);
 		if (playBoard.getIsOpen(yCordinate,xCordinate))
 		{
 			System.out.println(xCordinate + ":" + yCordinate);
@@ -142,8 +179,12 @@ public class GameBase {
 			break;
 		case 0:
 			playBoard.setOpen(yCordinate, xCordinate);
+			openSquareRecursive(xCordinate + 1, yCordinate + 1, playBoard);
 			openSquareRecursive(xCordinate + 1, yCordinate    , playBoard);
+			openSquareRecursive(xCordinate + 1, yCordinate - 1, playBoard);
+			openSquareRecursive(xCordinate - 1, yCordinate + 1, playBoard);
 			openSquareRecursive(xCordinate - 1, yCordinate    , playBoard);
+			openSquareRecursive(xCordinate - 1, yCordinate - 1, playBoard);
 			openSquareRecursive(xCordinate    , yCordinate + 1, playBoard);
 			openSquareRecursive(xCordinate    , yCordinate - 1, playBoard);
 			break;
--- src/minesweeper/PlayBoard.java
+++ src/minesweeper/PlayBoard.java
@@ -3,14 +3,26 @@ package minesweeper;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ *
+ * @author ryota
+ *
+ */
 public class PlayBoard {
+
 	private MapSymbol[][] mapArray;		// マップ
 	private int sideLength; 			// 一辺の長さ（下限は5,上限は20)
 	private int maxBombAmount;			// マップ上に設置できる爆弾の最大数
-	private int amountOfSafeSquare;		// 残っている爆弾の数
+	private int amountOfSafeSquare;		// マップ上の、爆弾を除いたマスの総数
 
+	// デフォルトコンストラクタ(引数ありを前提としているためprivate)
 	private PlayBoard(){}
 
+	/**
+	 * 引数ありコンストラクタ
+	 * @param sideLength	一辺の長さ
+	 * @param maxBombAmount	マップ上に設置できる爆弾の最大数
+	 */
 	public PlayBoard(int sideLength, int maxBombAmount)
 	{
 		this.sideLength         = sideLength;
@@ -20,22 +32,26 @@ public class PlayBoard {
 		InitMap();
 	}
 
+	// マップの初期化を行う
 	private void InitMap()
 	{
-
 		setBombOnMap();
 		calculateNumbers();
 	}
 
+	/**
+	 * クリア条件に達しているかどうかを判定して返す
+	 * @return true (クリアしている時) / false (クリアしていない時)
+	 */
 	public boolean isClear()
 	{
-		int amountOfOpenSquare = 0;
+		int amountOfOpenSquare = 0; /** 爆弾を除く、既にあいているマスの総数 */
 
-		for (int height = 0; height < this.sideLength; height++)
+		for (int yCordinate = 0; yCordinate < this.sideLength; yCordinate++)
 		{
-			for (int width = 0; width < this.sideLength; width++)
+			for (int xCordinate = 0; xCordinate < this.sideLength; xCordinate++)
 			{
-				if (this.mapArray[height][width].getIsOpen())
+				if (this.mapArray[yCordinate][xCordinate].getIsOpen())
 				{
 					amountOfOpenSquare++;
 				}
@@ -45,6 +61,7 @@ public class PlayBoard {
 		return (amountOfOpenSquare == (amountOfSafeSquare)) ? true : false;
 	}
 
+	// maxBombAmountの数だけマップに爆弾をセットする
 	private void setBombOnMap()
 	{
 		ArrayList<MapSymbol> mapSymbolList = new ArrayList<MapSymbol>();
@@ -61,49 +78,70 @@ public class PlayBoard {
 
 		Collections.shuffle(mapSymbolList);
 
-		for (int k = 0; k < this.sideLength; k++)
+		for (int yCordinate = 0; yCordinate < this.sideLength; yCordinate++)
 		{
-			for (int l = 0; l < this.sideLength; l++)
+			for (int xCordinate = 0; xCordinate < this.sideLength; xCordinate++)
 			{
-				this.mapArray[k][l] = mapSymbolList.get((k * this.sideLength) + l);
+				this.mapArray[yCordinate][xCordinate] = mapSymbolList.get((yCordinate * this.sideLength) + xCordinate);
 			}
 		}
 	}
 
+	// 隣接したマスに爆弾がいくつあるかを計算して挿入する
 	private void calculateNumbers()
 	{
-		for (int height = 0; height < this.sideLength; height++)
+		for (int yCordinate = 0; yCordinate < this.sideLength; yCordinate++)
 		{
-			for (int width = 0; width < this.sideLength; width++)
+			for (int xCordinate = 0; xCordinate < this.sideLength; xCordinate++)
 			{
-				if ( ! this.mapArray[height][width].isBomb())
+				if ( ! this.mapArray[yCordinate][xCordinate].isBomb())
 				{
 					int amountOfBombs = 0;
 
-					if (isBombExist(width + 1, height + 1)) amountOfBombs++;
-					if (isBombExist(width + 1, height - 1)) amountOfBombs++;
-					if (isBombExist(width + 1, height))     amountOfBombs++;
-					if (isBombExist(width - 1, height + 1)) amountOfBombs++;
-					if (isBombExist(width - 1, height - 1)) amountOfBombs++;
-					if (isBombExist(width - 1, height))     amountOfBombs++;
-					if (isBombExist(width    , height + 1)) amountOfBombs++;
-					if (isBombExist(width    , height - 1)) amountOfBombs++;
+					if (isBombExist(xCordinate + 1, yCordinate + 1)) amountOfBombs++;
+					if (isBombExist(xCordinate + 1, yCordinate - 1)) amountOfBombs++;
+					if (isBombExist(xCordinate + 1, yCordinate))     amountOfBombs++;
+					if (isBombExist(xCordinate - 1, yCordinate + 1)) amountOfBombs++;
+					if (isBombExist(xCordinate - 1, yCordinate - 1)) amountOfBombs++;
+					if (isBombExist(xCordinate - 1, yCordinate))     amountOfBombs++;
+					if (isBombExist(xCordinate    , yCordinate + 1)) amountOfBombs++;
+					if (isBombExist(xCordinate    , yCordinate - 1)) amountOfBombs++;
 
-					this.mapArray[height][width].setStatus(amountOfBombs);
+					this.mapArray[yCordinate][xCordinate].setStatus(amountOfBombs);
 				}
 			}
 		}
 	}
 
-	private boolean isBombExist(int width, int height)
+	/**
+	 * 指定された場所がマップの範囲内かどうかを判定して返す
+	 *
+	 * @param xCordinate x座標
+	 * @param yCordinate y座標
+	 * @return true (マップの範囲内の時) / false (マップの範囲外の時)
+	 */
+	public boolean isValidPosition(int xCordinate, int yCordinate)
 	{
-		if (width >= 0 && width <= this.sideLength - 1 && height >= 0 && height <= this.sideLength - 1)
+		if (xCordinate >= 0 && xCordinate <= this.sideLength - 1 && yCordinate >= 0 && yCordinate <= this.sideLength - 1)
 		{
-			return this.mapArray[height][width].isBomb();
+			return true;
 		}
 		return false;
 	}
 
+	private boolean isBombExist(int xCordinate, int yCordinate)
+	{
+		if (isValidPosition(xCordinate, yCordinate))
+		{
+			return this.mapArray[yCordinate][xCordinate].isBomb();
+		}
+		return false;
+	}
+
+	/**
+	 * マップを表示する
+	 * なお、爆弾マスは X 、旗が立っているマスは F 、まだ開かれていないマスは ? がそれぞれ表示される
+	 */
 	public void printMap()
 	{
 		char a = 'a';
@@ -114,31 +152,31 @@ public class PlayBoard {
 		}
 		System.out.println();
 
-		for (int height = 0; height < this.sideLength; height++)
+		for (int yCordinate = 0; yCordinate < this.sideLength; yCordinate++)
 		{
-			System.out.print(height + " ");
-			for (int width = 0; width < this.sideLength; width++)
+			System.out.print(yCordinate + " ");
+			for (int xCordinate = 0; xCordinate < this.sideLength; xCordinate++)
 			{
 				System.out.print("|");
-				if ( ! this.mapArray[height][width].getIsOpen())
+				if ( ! this.mapArray[yCordinate][xCordinate].getIsOpen())
 				{
 					System.out.print("?");
 					continue;
 				}
 
-				if (this.mapArray[height][width].getIsFlag())
+				if (this.mapArray[yCordinate][xCordinate].getIsFlag())
 				{
 					System.out.print("F");
 					continue;
 				}
 
-				if (this.mapArray[height][width].isBomb())
+				if (this.mapArray[yCordinate][xCordinate].isBomb())
 				{
 					System.out.print("X");
 				}
 				else
 				{
-					System.out.print(this.mapArray[height][width].getStatus());
+					System.out.print(this.mapArray[yCordinate][xCordinate].getStatus());
 				}
 
 			}
@@ -171,5 +209,10 @@ public class PlayBoard {
 		}
 		return -1;
 
+	}
+
+	public int getSideLength()
+	{
+		return this.sideLength;
 	}
 }
